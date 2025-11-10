f837a7dd414f91f56572c7e26c30f7bf95ff47dc
MeisterProepper <88040345+MeisterProepper@users.noreply.github.com>
2025-11-10 23:12:13 +0100

    Remove FIR_filtertest and unused shift register
    Deleted the FIR_filtertest function and the unused fir_shiftreg variable from FIR_HLS.cpp and FIR_HLS.h to clean up the codebase and remove obsolete or experimental code.

--- HLS_FIR/Direct_FIR_HLS/FIR_HLS.cpp
+++ HLS_FIR/Direct_FIR_HLS/FIR_HLS.cpp
@@ -12,38 +12,6 @@ void FIR_HLS(hls::stream<fir_data_t> &input, hls::stream<fir_data_t> &output){
 }
 
 
-
-/*
-fir_data_t FIR_filter(delay_data_t FIR_delays[], const coef_data_t FIR_coe[], int N_delays, fir_data_t x_n){
-    #pragma HLS PIPELINE
-	fir_data_t y;
-	ap_fixed<32,2> FIR_accu32=0;
-
-	FIR_delays[N_delays-1] = x_n;	// read input sample from ADC 
-	FIR_accu32	= 0;				// clear accu
-	for(int i=0; i < N_delays; i++)		// FIR filter routine
-		FIR_accu32 += FIR_delays[N_delays-1-i] * FIR_coe[i];
-
-	for(int i=1; i < N_delays; i++)				
-		FIR_delays[i-1] = FIR_delays[i];
- 
-	y = (fir_data_t) (FIR_accu32);
-	return y;
-}
-*/
-
-fir_data_t FIR_filtertest(delay_data_t FIR_delays[], const coef_data_t FIR_coe[], int N_delays, fir_data_t x_n){
-    #pragma HLS PIPELINE
-	fir_data_t y;
-
-    y = FIR_delays[0] + x_n * FIR_coe[0];
-
-	for(int i=1; i < N_delays; i++)		// FIR filter routine
-		FIR_delays[i-1] = FIR_delays[i] + FIR_coe[i] * x_n;
-
-	return y;
-}
-
 fir_data_t FIR_filter(delay_data_t FIR_delays[], const coef_data_t FIR_coe[], int N_delays, fir_data_t x_n){
     #pragma HLS PIPELINE
 	fir_data_t  y;
--- HLS_FIR/Direct_FIR_HLS/FIR_HLS.h
+++ HLS_FIR/Direct_FIR_HLS/FIR_HLS.h
@@ -6,10 +6,6 @@
 
 typedef ap_fixed<16,1> fir_data_t; 
 
-static ap_shift_reg<fir_data_t,N_DELAYS_FIR> fir_shiftreg;
-
 void FIR_HLS(hls::stream<fir_data_t> &input, hls::stream<fir_data_t> &output);
 
 fir_data_t FIR_filter(delay_data_t FIR_delays[], const coef_data_t FIR_coe[], int N_delays, fir_data_t x_n);
-
-fir_data_t FIR_filtertest(delay_data_t FIR_delays[], const coef_data_t FIR_coe[], int N_delays, fir_data_t x_n);
