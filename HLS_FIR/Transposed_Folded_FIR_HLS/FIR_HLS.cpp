9049fa6aefc663718eea6d5ae1811ac44820380d
MeisterProepper <88040345+MeisterProepper@users.noreply.github.com>
2025-11-12 15:29:01 +0100

    Transposed Folded FIR added

--- HLS_FIR/Transposed_Folded_FIR_HLS/FIR_HLS.cpp
+++ HLS_FIR/Transposed_Folded_FIR_HLS/FIR_HLS.cpp
@@ -9,29 +9,31 @@ void FIR_HLS(hls::stream<fir_data_t> &input, hls::stream<fir_data_t> &output){
     #pragma HLS INTERFACE mode=ap_ctrl_none port=return
 
 
-    output.write(FIR_filter(H_filter_FIR, b_FIR, N_DELAYS_FIR,input.read()));
+    output.write(FIR_filter(H_accu_FIR1, b_FIR, N_DELAYS_FIR,input.read()));
 }
 
 
-
-
-
-fir_data_t FIR_filter(delay_data_t FIR_delays[], const coef_data_t FIR_coe[], int N_delays, fir_data_t x_n){
+fir_data_t FIR_filter(accu_data_t FIR_delays1[], const coef_data_t FIR_coe[], int N_delays, fir_data_t x_n){
     #pragma HLS PIPELINE
+	fir_data_t y;
+    accu_data_t FIR_delays2[N_delays];
+    accu_data_t FIR_delays3[N_delays/2];
 
-	fir_data_t  y;
-	ap_fixed<32,1> FIR_accu32=0;
+    for(int i=0; i < (N_delays/2); i++){ // FIR filter routine
+		FIR_delays3[i] = FIR_coe[i] * x_n;
+    }		
 
-    FIR_delays[N_delays-1] = x_n;
+    y = FIR_delays1[0] + FIR_delays3[0];
 
+	for(int i=1; i < N_delays; i++){ // FIR filter routine
+		FIR_delays3[i] = FIR_coe[i] * x_n;
+        if (i < (N_delays/2)) {
+            FIR_delays1[i-1] = FIR_delays1[i] + FIR_delays3[i];
+        }
+        else {
+            FIR_delays1[i-1] = FIR_delays1[i] + FIR_delays3[N_delays-i-1];
+        }
+    }
 
-    for(int i= 0; i < ((N_delays/2) ); i++){
-		FIR_accu32 +=  FIR_coe[i] * (FIR_delays[i] + FIR_delays[N_delays -i-1]);
-		}
-
-    for(int i=1; i < N_delays; i++)					
-        FIR_delays[i-1] = FIR_delays[i];
-
-	y = FIR_accu32;
 	return y;
 }
---
+++ HLS_FIR/_ide/workspace_journal_8716.py
@@ -0,0 +1,179 @@
+# 2025-11-10T22:15:50.928526
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="HLS_FIR")
+
+comp = client.get_component(name="Direct_FIR_HLS")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="C_SIMULATION")
+
+comp = client.clone_component(name="Direct_FIR_HLS",new_name="Direct_FIR_SRL")
+
+comp.run(operation="SYNTHESIS")
+
+comp = client.get_component(name="Direct_FIR_SRL")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="SYNTHESIS")
+
+comp = client.clone_component(name="Direct_FIR_HLS",new_name="Transposed_FIR_HLS")
+
+comp = client.get_component(name="Transposed_FIR_HLS")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="SYNTHESIS")
+
+comp = client.clone_component(name="Transposed_FIR_HLS",new_name="Transposed_FIR_SRL")
+
+client.delete_component(name="Transposed_FIR_SRL")
+
+comp = client.clone_component(name="Direct_FIR_HLS",new_name="Folded_FIR_HLS")
+
+comp = client.get_component(name="Direct_FIR_DSP")
+comp.run(operation="SYNTHESIS")
+
+comp = client.get_component(name="Direct_FIR_HLS")
+comp.run(operation="SYNTHESIS")
+
+comp = client.get_component(name="Direct_FIR_SRL")
+comp.run(operation="SYNTHESIS")
+
+comp = client.get_component(name="Transposed_FIR_HLS")
+comp.run(operation="IMPLEMENTATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="IMPLEMENTATION")
+
+comp = client.get_component(name="Folded_FIR_HLS")
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp = client.get_component(name="Direct_FIR_DSP")
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="IMPLEMENTATION")
+
+comp = client.get_component(name="Direct_FIR_HLS")
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="IMPLEMENTATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="IMPLEMENTATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp = client.get_component(name="Direct_FIR_SRL")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="IMPLEMENTATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="IMPLEMENTATION")
+
+comp = client.get_component(name="Folded_FIR_HLS")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION_DEBUG")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp = client.get_component(name="Direct_FIR_DSP")
+comp.run(operation="C_SIMULATION")
+
+comp = client.get_component(name="Folded_FIR_HLS")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="IMPLEMENTATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="IMPLEMENTATION")
+
+comp = client.clone_component(name="Folded_FIR_HLS",new_name="Transposed_Folded_FIR_HLS")
+
+comp = client.get_component(name="Transposed_Folded_FIR_HLS")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp = client.clone_component(name="Folded_FIR_HLS",new_name="Folded_FIR_SRL")
+
+comp = client.get_component(name="Folded_FIR_SRL")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="SYNTHESIS")
+
+client.delete_component(name="Folded_FIR_SRL")
+
+comp = client.get_component(name="Transposed_Folded_FIR_HLS")
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="C_SIMULATION")
+
+comp.run(operation="SYNTHESIS")
+
